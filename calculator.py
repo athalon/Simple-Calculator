fd3941cb2032f4b1c61798db40cf634eb9a3e60f
athalon <58222464+athalon@users.noreply.github.com>
2020-03-25 16:25:18 +0100

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -52,60 +52,6 @@ while True:
         finally:
             sl(2)
 
-    from time import sleep as sl
-
-
-while True:
-    print("Options:")
-    print("Enter 'add' to add two numbers")
-    print("Enter 'subtract' to subtract two numbers")
-    print("Enter 'multiply' to multiply two numbers")
-    print("Enter 'divide' to divide two numbers")
-    print("Enter 'power' to raise a number to the power of another number")
-    print("Enter 'modulo' to calculate the modulo of two numbers")
-    print("Enter 'quit' to end the program")
-    user_input = input(": ")
-
-    if user_input == "quit":
-        break
-
-    elif user_input == "add":
-        try:
-            num1 = float(input("Enter a number: "))
-            num2 = float(input("Enter another number: "))
-            print(f"The answer is {num1 + num2}")
-            print(".")
-            print(".")
-        except OverflowError:
-            print("Error, result is too large!")
-        finally:
-            sl(2)
-
-    elif user_input == "subtract":
-        try:
-            num1 = float(input("Enter a number: "))
-            num2 = float(input("Enter another number: "))
-            print(f"The answer is {num1 - num2}")
-            print(".")
-            print(".")
-
-        except OverflowError:
-            print("Error, result is too large!")
-        finally:
-            sl(2)
-
-    elif user_input == "multiply":
-        try:
-            num1 = float(input("Enter a number: "))
-            num2 = float(input("Enter another number: "))
-            print(f"The answer is {num1 * num2}")
-            print(".")
-            print(".")
-        except OverflowError:
-            print("Error, result is too large!")
-        finally:
-            sl(2)
-
     elif user_input == "divide":
         try:
             num1 = float(input("Enter a number: "))
